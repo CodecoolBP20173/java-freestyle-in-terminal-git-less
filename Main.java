533d23a060a0497d2f46d09d338ee6bc8f1839fd
Oláh Tamás <tolah94@gmail.com>
2018-03-06 17:01:46 +0100

    Basic player turns (not implemented on backTable)

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void clearField () {
+    static void clearField() {
 
     }
 
     static String[][] createFrontTable() {
-        String[][] frontTable = new String [6][7];
+        String[][] frontTable = new String[6][7];
         for (int i = 0; i < frontTable.length; i++) {
             for (int j = 0; j < frontTable[0].length; j++) {
                 frontTable[i][j] = "::";
@@ -18,15 +18,16 @@ public class Main {
     }
 
     static String[][] createBackTable() {
-        String[][] backTable = new String [6][7];
+        String[][] backTable = new String[6][7];
 
         return backTable;
     }
 
-    static void printTable(String[][] table){
+    static void printTable(String[][] table) {
+        System.out.println("  1    2    3    4    5    6    7");
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table[0].length; j++) {
-                if (j == 0){
+                if (j == 0) {
                     System.out.print("| ");
                 }
                 System.out.print(table[i][j] + " | ");
@@ -42,10 +43,48 @@ public class Main {
         return move;
     }
 
-    static void displayCircleForPlayerOne() {
+    static void displayCircleForPlayerOne(String[][] table) {
+        System.out.println("Red Player's turn!");
+        Scanner reader = new Scanner(System.in);
+        int columnToDrop;
+        do {
+            System.out.println("Choose a column (1-7):");
+            while (!reader.hasNextInt()) {
+                System.out.println("That's not a number!");
+                System.out.println("Choose a column (1-7):");
+                reader.next();
+            }
+            columnToDrop = reader.nextInt();
+        } while (columnToDrop <= 0 || columnToDrop >= 8);
+        --columnToDrop;
+        for (int i = 5; i >= 0; i--) {
+            if (table[i][columnToDrop] == "::") {
+                table[i][columnToDrop] = "R ";
+                break;
+            }
+        }
     }
 
-    static void displayCircleForPlayerTwo() {
+    static void displayCircleForPlayerTwo(String[][] table) {
+        System.out.println("Blue Player's turn!");
+        Scanner reader = new Scanner(System.in);
+        int columnToDrop;
+        do {
+            System.out.println("Choose a column (1-7):");
+            while (!reader.hasNextInt()) {
+                System.out.println("That's not a number!");
+                System.out.println("Choose a column (1-7):");
+                reader.next();
+            }
+            columnToDrop = reader.nextInt();
+        } while (columnToDrop <= 0 || columnToDrop >= 8);
+        --columnToDrop;
+        for (int i = 5; i >= 0; i--) {
+            if (table[i][columnToDrop] == "::") {
+                table[i][columnToDrop] = "B ";
+                break;
+            }
+        }
     }
 
     static boolean winCheck() {
@@ -59,5 +98,21 @@ public class Main {
         printTable(frontTable);
         System.out.print("\n");
         printTable(backTable);
+        displayCircleForPlayerOne(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerTwo(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerOne(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerTwo(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerOne(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerTwo(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerOne(frontTable);
+        printTable(frontTable);
+        displayCircleForPlayerTwo(frontTable);
+        printTable(frontTable);
     }
 }
